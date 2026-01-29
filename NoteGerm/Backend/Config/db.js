c54a8406fafeb285b86db18ea5bf2b6fde7f4261
Ankit <ankit@9e190481-992e-443e-a2ea-9f42238589af.fritz.box>
2026-01-29 15:24:26 +0100

    mongodb atlas instead of local

--- NoteGerm/Backend/Config/db.js
+++ NoteGerm/Backend/Config/db.js
@@ -1,15 +1,36 @@
 //mongodb+srv://rahulverma:rahul@cluster0.fpxfh.mongodb.net/Notes
 
+// import mongoose from "mongoose";
+
+// const connectDb = async () => {
+//   try {
+//     await mongoose.connect("mongodb://localhost:27017");
+//     console.log("MongoDB connected");
+//   } catch (err) {
+//     console.error("MongoDB connection error:", err);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDb;
+
+
+
 import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017");
-    console.log("MongoDB connected");
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not found in environment variables");
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected ✅");
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
 
 export default connectDb;
+
